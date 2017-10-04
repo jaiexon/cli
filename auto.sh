995a7c847a92c617a2c42c70342498fcfb3fd56c
liteTeam <iranpc.90@gmail.com>
2017-10-04 08:03:14 -0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+COUNTER=1
+while(true) do
+./parsol.sh
+curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
+-- @Lite_Team
